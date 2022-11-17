971a53d7d364d80743575b89fbe1bcbd38b9ecc8
MotasemZiad <miziad2000@gmail.com>
2022-11-17 18:34:09 +0200

    create gitignore file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Data Structures and Algorithms using Java!");
     }
 }
