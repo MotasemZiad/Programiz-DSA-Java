69806391debafaf306e1dbaf34acd293d103d77f
MotasemZiad <miziad2000@gmail.com>
2022-12-20 14:01:16 +0200

    some updates..

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Data Structures and Algorithms using Java!");
-        System.out.println(
-                "Success is like being pregnant, everyone says congratulation but nobody knows how many times you got fucked.");
     }
 }
