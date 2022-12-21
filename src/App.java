f0e9acbddf9a79868a2631b41722751fb22e5094
MotasemZiad <miziad2000@gmail.com>
2022-12-21 14:29:16 +0200

    Return to the previous one.

--- src/App.java
+++ src/App.java
@@ -1,19 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Data Structures and Algorithms using Java!");
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = i; j < 10; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
     }
 }
